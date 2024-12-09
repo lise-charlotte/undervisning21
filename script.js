b297cbaf32c8dbefd274a7291486d540a81cdcaa
lise-charlotte <lisecharlottekoder@gmail.com>
2024-12-09 10:22:04 +0100

    Class starting

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// For Loops
 
+for (
+    let i = 0; // Declares a variable that starts with 0
+    i < 5; // Checks if variable is less than 5
+    i++ // Adds the index + index to count an iteration (iteration=repetition)
+    ) {
+        console.log("Hello World!");
+
+}
